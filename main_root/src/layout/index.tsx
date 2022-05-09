f970c9d4e95fa31a618614087c22484e170d8c87
kongdesheng <kongdesheng@focusmedia.cn>
2022-05-09 21:16:45 +0800

    feat: siderBar updagte

--- main_root/src/layout/index.tsx
+++ main_root/src/layout/index.tsx
@@ -1,35 +1,46 @@
 import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
-import { useHistory } from "umi"
+import { useHistory } from 'umi';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   ClusterOutlined,
-  ApartmentOutlined
+  ApartmentOutlined,
 } from '@ant-design/icons';
 import './index.less';
 
 const { Header, Sider, Content } = Layout;
 const { SubMenu } = Menu;
 
-export default function LayoutPage(props:any) {
-  const history = useHistory()
+export default function LayoutPage(props: any) {
+  const history = useHistory();
   const [collapsed, setCollapsed] = useState<boolean>(false);
   const toggle = () => {
     setCollapsed(!collapsed);
   };
 
-  const [selectedMenuKey,setSelectedMenuKey] = useState<Array<string>>([history.location.pathname]);
+  const [selectedMenuKey, setSelectedMenuKey] = useState<Array<string>>([
+    history.location.pathname,
+  ]);
 
-  const menuChange = (router: { key: string; })=>{
-    history.push(router.key)
-    console.log(history)
-    setSelectedMenuKey([router.key])
-  }
+  const menuChange = (router: { key: string }) => {
+    history.push(router.key);
+    console.log(history);
+    setSelectedMenuKey([router.key]);
+  };
 
   return (
     <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+        }}
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        theme="dark"
+      >
         <div className="logo">
           <img
             src="https://gw.alipayobjects.com/zos/bmw-prod/8a74c1d3-16f3-4719-be63-15e467a68a24/km0cv8vn_w500_h500.png"
@@ -37,36 +48,25 @@ export default function LayoutPage(props:any) {
             title="微前端(qiankun + umi)"
           />
         </div>
-        <Menu theme="dark" mode="inline"   selectedKeys={selectedMenuKey} onClick={menuChange}>
-          <SubMenu
-            key="main"
-            icon={<ClusterOutlined />}
-            title="主应用"
-          >
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={selectedMenuKey}
+          onClick={menuChange}
+        >
+          <SubMenu key="main" icon={<ClusterOutlined />} title="主应用">
             <Menu.Item key="/main/first">主应用页面1</Menu.Item>
             <Menu.Item key="/main/second">主应用页面2</Menu.Item>
           </SubMenu>
-          <SubMenu
-            key="sub"
-            icon={<ApartmentOutlined />}
-            title="vue2.0子应用"
-          >
+          <SubMenu key="sub" icon={<ApartmentOutlined />} title="vue2.0子应用">
             <Menu.Item key="/vue2/">子应用1_1</Menu.Item>
             <Menu.Item key="/vue2/about">子应用1_2</Menu.Item>
           </SubMenu>
-          <SubMenu
-            key="sub2"
-            icon={<ApartmentOutlined />}
-            title="vue3.0子应用"
-          >
+          <SubMenu key="sub2" icon={<ApartmentOutlined />} title="vue3.0子应用">
             <Menu.Item key="/vue3/">子应用2_1</Menu.Item>
             <Menu.Item key="/vue3/about">子应用2_2</Menu.Item>
           </SubMenu>
-          <SubMenu
-            key="react"
-            icon={<ApartmentOutlined />}
-            title="react子应用"
-          >
+          <SubMenu key="react" icon={<ApartmentOutlined />} title="react子应用">
             <Menu.Item key="/react/">子应用</Menu.Item>
           </SubMenu>
           <SubMenu
@@ -77,11 +77,7 @@ export default function LayoutPage(props:any) {
             <Menu.Item key="/purehtml">HTML子应用</Menu.Item>
           </SubMenu>
           {/* http://localhost:8003/ */}
-          <SubMenu
-            key="umi"
-            icon={<ApartmentOutlined />}
-            title="Umi子应用"
-          >
+          <SubMenu key="umi" icon={<ApartmentOutlined />} title="Umi子应用">
             <Menu.Item key="/umi">umi子应用</Menu.Item>
           </SubMenu>
 
